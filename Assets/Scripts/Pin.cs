3cb9736458db2fea11f26c506205f3ed06d52992
Ludwig Huang <0711feiyu@gmail.com>
2023-07-15 15:29:08 +0800

    feat: add select and internal length function into pins

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -1,35 +1,63 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Pin : MonoBehaviour
 {
     // Once created, immediately render 4*4 planes, and hold their heights with initial 0
-    public Tuple<Transform, int>[] pins;
+    public Tuple<Transform, int>[] Pins;
+    public List<Tuple<Transform, int>> SelectedPins;
 
     // Start is called before the first frame update
     void Start()
     {
-        int i = 0;
-        pins = new Tuple<Transform, int>[16];
+        var i = 0;
+        Pins = new Tuple<Transform, int>[16];
+        SelectedPins = new List<Tuple<Transform, int>>();
         foreach (Transform child in transform)
         {
-            pins[i] = new Tuple<Transform, int>(child, 0);
+            Pins[i] = new Tuple<Transform, int>(child, 0);
             i++;
-        }
-
-        foreach (var pin in pins)
-        {
-            Debug.Log(pin.Item1 + " === " + pin.Item2);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < 16; i++)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit) &&
+                hit.collider.gameObject == Pins[i].Item1.gameObject)
+            {
+                ChangeColor(i);
+                Debug.Log("SelectedPins Length: " + SelectedPins.Count);
+            }
+        }
     }
 
+    // Change color [single pin]
+    void ChangeColor(int i)
+    {
+        var renderer = Pins[i].Item1.GetComponent<MeshRenderer>();
+        var materials = new List<Material>();
+        renderer.GetMaterials(materials);
+
+        Assert.AreEqual(materials.Count, 1);
+        if (materials[0].color == Color.black)
+        {
+            materials[0].color = Color.cyan;
+            SelectedPins.Add(Pins[i]);
+        }
+        else
+        {
+            Assert.IsTrue(materials[0].color == Color.cyan);
+            materials[0].color = Color.black;
+            SelectedPins.Remove(Pins[i]);
+        }
+
+        renderer.SetMaterials(materials);
+    }
 }
